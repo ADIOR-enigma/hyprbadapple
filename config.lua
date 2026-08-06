d132930b5b610129033d9dded38cc9aa19ae63fe
adior <157355801+ADIOR-enigma@users.noreply.github.com>
2026-08-07 00:08:00 +0530

    chore: update rendering configuration parameters for box count, scale, and positioning

--- config.lua
+++ config.lua
@@ -5,10 +5,10 @@ return {
 	BOX_PATH   = "baprocess/output/boxes.bin", -- path to the generated bin file
 	AUDIO_PATH = "baprocess/badapple.mp3",
 
-	MAX_BOXES = 500, -- the `most boxes` from python
+	MAX_BOXES = 373, -- the `most boxes` from python
 	FPS       = 30, -- 1-30 framerate (sync playback)
 
-	SCALE    = 10, -- scale  -> resolution height / (grid height)
-	OFFSET_X = 320, -- center -> [(resolution width) - (grid width * scale)] / 2
-	OFFSET_Y = 60, -- depends on your top|bottom bars - can be 0 if you want it to fullscreen
+	SCALE    = 11, -- scale  -> resolution height / (grid height)
+	OFFSET_X = 256, -- center -> [(resolution width) - (grid width * scale)] / 2
+	OFFSET_Y = 12, -- depends on your top|bottom bars - can be 0 if you want it to fullscreen
 };
